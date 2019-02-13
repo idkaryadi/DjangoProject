cf4a5efce95cd064b23a0b905216e1af27bfd7b6
idkaryadi <karyadi757@gmail.com>
2019-02-13 19:25:37 +0700

    first-blog

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.blog, name = 'blog')
+    path('', views.blog, name = 'blog'),
+    path('post_new/', views.input_post, name='post_new'),
+    path('post/<int:post_id>/', views.post_detail, name = 'post_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Blog
+from .forms import BlogForm
 
 # Create your views here.
 def blog(request):
     blog = Blog.objects.all()
     return render(request, 'blog/blog.html', {'blogs':blog})
+
+def post_detail(request, post_id):
+    blog = Blog.objects.get(pk = post_id)
+    return render(request, 'blog/post_detail.html', {'blog':blog})
+
+def input_post(request):
+    if request.method == 'POST':
+        form = BlogForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect(blog) #
+    
+    else:
+        form = BlogForm()
+    return render(request, 'blog/post_new.html', {'form':form})
